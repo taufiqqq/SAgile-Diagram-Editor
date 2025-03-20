9e825665c0f2e12786ed4aad1bdb910f0a2c752b
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2025-03-20 11:04:06 +0800

    update

---
+++ src/controller/CustomNodeFlow.tsx
@@ -0,0 +1,30 @@
+import React, { useCallback } from 'react';
+import {
+  ReactFlow,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from '@xyflow/react';
+
+import '@xyflow/react/dist/style.css';
+import { initialNodes, initialEdges } from '../parser/GenerateNodes'; // Importing initial nodes and edges
+
+export default function CustomNodeFlow() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+    />
+  );
+}
