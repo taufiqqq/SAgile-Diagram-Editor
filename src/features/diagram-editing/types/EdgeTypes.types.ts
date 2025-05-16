cd72f0dd42673a935b29dcec15e2c6fb63650c7f
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2025-05-16 09:29:07 +0800

    ui

--- src/features/diagram-editing/types/EdgeTypes.types.ts
+++ src/features/diagram-editing/types/EdgeTypes.types.ts
@@ -1,7 +1,8 @@
 import { Edge } from '@xyflow/react';
 import CustomEdge from '../components/CustomEdge';
+import GeneralizationEdge from '../components/GeneralizationEdge';
 
-export type EdgeType = 'association' | 'include' | 'exclude';
+export type EdgeType = 'association' | 'include' | 'exclude' | 'generalization';
 
 export interface EdgeData {
   type: EdgeType;
@@ -13,7 +14,8 @@ export type CustomEdgeType = Edge<EdgeData>;
 export const edgeTypes = {
   default: CustomEdge,
   straight: CustomEdge,
-  custom: CustomEdge
+  custom: CustomEdge,
+  generalization: GeneralizationEdge
 };
 
 export type EdgeTypeName = keyof typeof edgeTypes; 
